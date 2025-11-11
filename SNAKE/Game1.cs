1cc993dbc2275ad54c5e03818da07e7c9536f4d4
Jakub Blažík <blazikj24@spstrutnov.cz>
2025-11-11 10:56:13 +0100

    hráč se prodlužuje a nemůže vyjet ze základní plochy

--- SNAKE/Game1.cs
+++ SNAKE/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace SNAKE
 {
@@ -8,12 +10,16 @@ namespace SNAKE
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Had hrac;
+        private List<Had> jablka;
+        private Random random;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            random = new Random();
         }
 
         protected override void Initialize()
@@ -26,8 +32,14 @@ namespace SNAKE
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            hrac = new Had(GraphicsDevice, new Rectangle(100, 100, 50, 50));
+            jablka = new List<Had>();
+            for (int i = 0; i < 10; i++)
+            {
+                int x = random.Next(0, _graphics.PreferredBackBufferWidth - 20);
+                int y = random.Next(0, _graphics.PreferredBackBufferHeight - 20);
+                jablka.Add(new Had(GraphicsDevice, new Rectangle(x, y, 20, 20)));
+            }
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,16 +47,30 @@ namespace SNAKE
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
+            KeyboardState state = Keyboard.GetState();
+            hrac.Pohnout(state, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
+            for (int i = jablka.Count - 1; i >= 0; i--)
+            {
+                if (hrac.Koliduje(jablka[i]))   
+                {
+                    jablka.RemoveAt(i);
+                    hrac.PridatSegment();
+                }
+            }
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            foreach (var jablko in jablka)
+            {
+                jablko.Draw(_spriteBatch, Color.Green);
+            }
+            hrac.Draw(_spriteBatch, Color.Red);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ SNAKE/had.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+
+namespace SNAKE
+{
+    internal class Had
+    {
+        private Texture2D texture;
+        public Rectangle Rect { get; private set; }
+        private List<Rectangle> telo; 
+        private List<Vector2> predchoziPozice;
+
+        int v = 5;
+        public Had(GraphicsDevice graphicsDevice, Rectangle startRect)
+        {
+            Rect = startRect;
+            texture = new Texture2D(graphicsDevice, 1, 1);
+            texture.SetData(new[] { Color.White });
+            telo = new List<Rectangle>();
+            predchoziPozice = new List<Vector2>();
+        }
+        public void Pohnout(KeyboardState state,int velikostOknaX,int VelikostOknaY)
+        {
+            predchoziPozice.Insert(0, new Vector2(Rect.X, Rect.Y));
+
+            int x = Rect.X;
+            int y = Rect.Y;
+
+            if (state.IsKeyDown(Keys.A))
+                x -= v;
+            if (state.IsKeyDown(Keys.D))
+                x += v;
+            if (state.IsKeyDown(Keys.W))
+                y -= v;
+            if (state.IsKeyDown(Keys.S))
+                y += v;
+            if (x < 0)
+                x = velikostOknaX;
+            if (x > velikostOknaX)
+                x = 0;
+            if (y < 0)
+                y = VelikostOknaY;
+            if (y > VelikostOknaY)
+                y = 0;
+
+
+            Rect = new Rectangle(x, y, Rect.Width, Rect.Height);
+
+            for (int i = 0; i < telo.Count; i++)
+            {
+                if (i < predchoziPozice.Count)
+                {
+                    telo[i] = new Rectangle(
+                        (int)predchoziPozice[i].X,
+                        (int)predchoziPozice[i].Y,
+                        telo[i].Width,
+                        telo[i].Height
+                    );
+                }
+            }
+
+
+            if (predchoziPozice.Count > telo.Count + 50)
+            {
+                predchoziPozice.RemoveAt(predchoziPozice.Count - 1);
+            }
+        }
+
+        public void PridatSegment()
+        {
+
+            Rectangle novySegment = new Rectangle(Rect.X, Rect.Y, Rect.Width, Rect.Height);
+            telo.Add(novySegment);
+        }
+        public void zvetsit(int oKolik)
+        {
+            Rect = new Rectangle(Rect.X, Rect.Y, Rect.Width + oKolik, Rect.Height + oKolik);
+        }
+        public void zpomalit()
+        {
+            v = (int)(v * 0.9);
+            if (v < 1) v = 1;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Color color)
+        {
+            Color teloBarva = new Color(
+                (int)(color.R * 0.6f),
+                (int)(color.G * 0.6f),
+                (int)(color.B * 0.6f)
+            );
+
+            foreach (var segment in telo)
+            {
+                spriteBatch.Draw(texture, segment, teloBarva);
+            }
+
+            spriteBatch.Draw(texture, Rect, color);
+        }
+        public bool Koliduje(Had jiny)
+        {
+            return Rect.Intersects(jiny.Rect);
+        }
+    }
+}
